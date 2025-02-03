870c1c11a2a3647fb99f118863b2f87c0d21ef56
JayGopani0 <jaygopani0@gmail.com>
2025-02-03 23:51:18 +0000

    newbot

---
+++ main.py
@@ -0,0 +1,10 @@
+def greeting(string name): 
+    print(f'Hello {name}, what can I do for you today?')
+
+def choice(string action):
+    if action == "returning":
+        pass
+    if action = "exchanging":
+        pass
+
+
